fe846b644f55a9571308976f473c05b40c7b6cd5
Lucasmsl00 <lucaslima131@gmail.com>
2024-10-02 15:20:49 -0300

    apagando estilizações desnecessárias do menu_style e corrigindo erro do text-decoration dos links do botão

--- includes/menu.php
+++ includes/menu.php
@@ -1,4 +1,4 @@
-<button class="a_menu"><a href="<?= constant('URL_LOCAL_SITE_PAGINA').'login'?>">Login</a></button>
-<button class="a_menu"><a href="<?= constant('URL_LOCAL_SITE_PAGINA').'registro'?>">Registro</a></button>
-<button class="a_menu"><a href="<?= constant('URL_LOCAL_SITE_PAGINA').'contato'?>">Contato</a></button>
-<button class="a_menu"><a href="<?= constant('URL_LOCAL_SITE_PAGINA').'noticia'?>">Criar Notícia</a></button>
+<button><a class="a_menu" href="<?= constant('URL_LOCAL_SITE_PAGINA').'login'?>">Login</a></button>
+        <button><a class="a_menu" href="<?= constant('URL_LOCAL_SITE_PAGINA').'registro'?>">Registro</a></button>
+        <button><a class="a_menu" href="<?= constant('URL_LOCAL_SITE_PAGINA').'contato'?>">Contato</a></button>
+        <button><a class="a_menu" href="<?= constant('URL_LOCAL_SITE_PAGINA').'noticia'?>">Criar Notícia</a></button>
